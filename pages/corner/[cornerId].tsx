bd953c8d17080ade828d8bea07977f9c52b2bdff
Felix Thape <felix.thape@gmail.com>
2024-09-28 10:32:33 +0200

    feat: add LoadingSpinner and backbutton

--- pages/corner/[cornerId].tsx
+++ pages/corner/[cornerId].tsx
@@ -5,6 +5,8 @@ import Timebar from "@/components/Timebar";
 import BarChart from "@/components/BarChart";
 import {useLoadCornerDetails} from "@/api/useLoadCornerDetails";
 import {Meassurement} from "@/api/types";
+import {ChevronLeft} from "lucide-react";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 
 const CornerDetailsPage: NextPage = () => {
@@ -13,15 +15,26 @@ const CornerDetailsPage: NextPage = () => {
 
   const {corner, isLoading} = useLoadCornerDetails(id)
 
-  if (isLoading) return <p>Loading...</p>
-  if (!corner || !id) return <p>No profile data</p>
+  if (isLoading) return <LoadingSpinner/>
+  if (!corner) return <LoadingSpinner/>
 
-  const chartData = corner?.noise_value_hour.map((measurement: Meassurement) => measurement.value)
-  const chartDataLabels = corner?.noise_value_hour.map(() => "")
+  const chartData = corner.noise_value_hour.map((measurement: Meassurement) => measurement.value)
+  const chartDataLabels = corner.noise_value_hour.map(() => "")
 
   return (
     <div className={"flex flex-col h-screen bg-black items-center p-5 gap-y-8"}>
-      <h1 className={"text-3xl font-bold"}>{corner?.name}</h1>
+      <div className={"flex flex-row gap-x-4 w-full justify-between"}>
+        <button
+          className={"flex flex-row items-center justify-center w-7 h-7 rounded-full bg-white text-black"}
+          onClick={() => {
+            router.push("/")
+          }}
+        >
+          <ChevronLeft size={24} strokeWidth={3} className={"-translate-x-[1px]"}/>
+        </button>
+        <h1 className={"text-3xl font-bold"}>{corner?.name}</h1>
+        <div className={"w-7 h-7 min-w-[28px]"}/>
+      </div>
       <Timebar/>
       <NoiseIndicator sliceCount={25} percentage={corner?.noise_value ?? 0} max={100}/>
       <div className={"w-full h-full max-w-[500px]"}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import {useLoadCorners} from "@/api/useLoadCorners";
 import {useRouter} from "next/router";
 import {MarkerProps} from "@/components/Marker";
 import {colorToHex, priceFactorToColor} from "@/util/noiseValueToColor";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 const DynamicMapComponent = dynamic(() => import('../components/Map'), {
   ssr: false, // Disable SSR for this component
@@ -14,20 +15,19 @@ const IndexPage: NextPage = () => {
   const router = useRouter()
 
   if (data === undefined) {
-    // TODO improve
-    return <div>Laden...</div>
+    return <LoadingSpinner/>
   }
 
   const mappedData: MarkerProps[] = data.map(corner => ({
     position: corner.position,
-    iconColor: colorToHex(priceFactorToColor(corner.priceFactor)),
+    iconColor: colorToHex(priceFactorToColor(corner.price_factor)),
     children: (
       <div className={"flex flex-col gap-y-2"}>
         <h6 className={"text-lg font-bold"}>{corner.name}</h6>
         <button onClick={() => router.push(`/corner/${corner.id}`)}>{"Auswählen"}</button>
       </div>
     ),
-    priceFactor: corner.priceFactor
+    priceFactor: corner.price_factor
   }))
 
   return (
