c4f7a89a69858871d0750b089d4c320a2f7c8fd3
Felix <67233923+DasProffi@users.noreply.github.com>
2024-09-28 15:32:03 +0200

    feat: update corner page, marker and info text (#16)

--- components/MarkerIcon.tsx
+++ components/MarkerIcon.tsx
@@ -8,13 +8,15 @@ type MarkerIconProps = {
 export const MarkerIcon = ({color = "red", scaleFactor}: MarkerIconProps) => {
   return L.divIcon({
     className: 'custom-marker',
-    html: `<div class="brightness-150 relative w-[29px] h-[45px] min-w-[29px] min-h-[45px]">
-        <svg width="29" height="45" viewBox="0 0 29 45" xmlns="http://www.w3.org/2000/svg">
-          <path fill=${color} d="M14.5 0C22.75 0 29 6.25 29 14.5c0 10.25-14.5 28.5-14.5 28.5S0 24.75 0 14.5C0 6.25 6.25 0 14.5 0z"/>
-          <circle cx="14.5" cy="14.5" r="11.5" fill="white"/>
-        </svg>
-        <div class="absolute text-[${color}] font-black text-center top-[6px] w-full text-xs font-mono">${scaleFactor.toFixed(1)}</div>
-      </div>`,
+    html: `<div class="brightness-150 relative w-[40px] h-[52px] min-w-[40px] min-h-[52px]">
+  <svg width="40" height="52" viewBox="0 0 40 52" xmlns="http://www.w3.org/2000/svg">
+    <path fill="${color}" d="M20 0C31.05 0 40 8.95 40 20S20 52 20 52 0 31.05 0 20 8.95 0 20 0z"/>
+    <circle cx="20" cy="20" r="15" fill="white"/>
+  </svg>
+  <div class="absolute text-[${color}] font-bold text-center top-[11px] w-full font-mono" style="filter: brightness(0.5)">
+    ${scaleFactor.toFixed(1)}
+  </div>
+</div>`,
     iconSize: [25, 41],
     iconAnchor: [12.5, 41],
     popupAnchor: [1, -34],
