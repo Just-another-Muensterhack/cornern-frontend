5311f6fccd905ee081c0bf3afd7291eb191f15a3
Felix Evers <git@felixevers.de>
2024-09-28 11:17:20 +0200

    chore: update naming to corndex

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="og:title" property="og:title" content="Cornern" />
-          <meta name="description" content="Cornern" />
-          <meta property="og:description" content="Cornern" />
+          <meta name="og:title" property="og:title" content="corndex" />
+          <meta name="description" content="corndex" />
+          <meta property="og:description" content="corndex" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
-          <meta property="og:site_name" content="Cornern" />
+          <meta property="og:site_name" content="corndex" />
           <meta property="og:type" content="website" />
           <meta property="og:locale" content="de_DE" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet"/>
